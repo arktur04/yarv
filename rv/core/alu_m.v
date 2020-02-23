aec28183e5eaa3111f56e533d0559137448b4026
VT <arktur04@gmail.com>
2020-02-23 21:16:37 +0300

    to do: alu_m.v

--- rv/core/alu_m.v
+++ rv/core/alu_m.v
@@ -18,7 +18,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 module alu_i 
 #(
-  parameter INSTR_WIDTH = 4,
+  parameter INSTR_WIDTH = 3,
   parameter DATA_WIDTH = 32
 )
 (
@@ -29,19 +29,18 @@ module alu_i
   input wire [DATA_WIDTH - 1: 0] rs2,
   input wire [INSTR_WIDTH - 1: 0] operation,
   // result
-  output reg [DATA_WIDTH - 1: 0] rd
+  output reg [DATA_WIDTH - 1: 0] rd1,
+  output reg [DATA_WIDTH - 1: 0] rd2,
 );
 
-  localparam INSTR_ADD = 0;
-  localparam INSTR_SLT = 1;
-  localparam INSTR_SLTU = 2;
-  localparam INSTR_AND = 3;
-  localparam INSTR_OR = 4;
-  localparam INSTR_XOR = 5;
-  localparam INSTR_SLL = 6;
-  localparam INSTR_SRL = 7;
-  localparam INSTR_SUB = 8;
-  localparam INSTR_SRA = 9;
+  localparam INSTR_MUL = 0;
+  localparam INSTR_MULH = 1;
+  localparam INSTR_MULHU = 2;
+  localparam INSTR_MULHSU = 3;
+  localparam INSTR_DIV = 4;
+  localparam INSTR_DIVU = 5;
+  localparam INSTR_REM = 6;
+  localparam INSTR_REMU = 7;
   
   always@(negedge rst_n, posedge clk)
   if(!rst_n)
@@ -50,22 +49,14 @@ module alu_i
   else
     begin
       case (operation)
-        INSTR_ADD: rd <= rs1 + rs2;
-        INSTR_SLT: if($signed(rs1) < $signed(rs2))
-            rd <= 1'b1;
-          else
-            rd <= 1'b0;
-        INSTR_SLTU: if(rs1 < rs2)
-            rd <= 1'b1;
-          else
-            rd <= 1'b0;
-        INSTR_AND: rd <= rs1 & rs2;
-        INSTR_OR: rd <= rs1 | rs2;
-        INSTR_XOR: rd <= rs1 ^ rs2;
-        INSTR_SLL: rd <= rs1 << rs2;
-        INSTR_SRL: rd <= rs1 >> rs2;
-        INSTR_SUB: rd <= rs1 - rs2;
-        INSTR_SRA: rd <= rs1 >>> rs2;
+        INSTR_MUL: 
+        INSTR_MULH: {rd1, rd2} <= rs1 * rs2; 
+        INSTR_MULHU:
+        INSTR_MULHSU: 
+        INSTR_DIV: 
+        INSTR_DIVU: 
+        INSTR_REM: 
+        INSTR_REMU: {rd1, rd2} <= 0; // todo
         default: rd <= 0; // todo: check if we can remove this case for an optimization purpose
       endcase      
     end
